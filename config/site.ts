a34b4edd2cd9a4a0f6f6a602710d081f7fc84ba1
Dylan Watson <lotsabackscatter@gmail.com>
2022-04-28 23:31:37 +0800

    More personalisation

--- config/site.ts
+++ config/site.ts
@@ -1,17 +1,17 @@
 const siteConfig = {
   details: {
-    title: "Nirmalya Ghosh",
+    title: "Dylan Watson",
     tagLine: "Sofware Developer",
     description:
       "Articles and guides about JavaScript, CSS and Web Development from a software engineer.",
-    url: "https://www.nirmalyaghosh.com",
+    url: "https://dylanwatsonsoftware.vercel.app",
   },
   assets: {
     avatar: "/images/common/seo-cover.png",
     favicon: "/images/common/favicon.svg",
   },
   socialLinks: {
-    twitter: "@nirmalyaghosh23",
+    twitter: "@dylanwatsonsw",
   },
 };
 
