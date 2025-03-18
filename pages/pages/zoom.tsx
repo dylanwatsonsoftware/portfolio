bbecb0f039fb2436f1362e14018913e44376edad
Dylan Watson <lotsabackscatter@gmail.com>
2025-03-18 12:09:03 +0800

    Add a title

--- pages/pages/zoom.tsx
+++ pages/pages/zoom.tsx
@@ -29,12 +29,17 @@ const ZoomStatusPage = ({ onZoomCall }: { onZoomCall: string | null }) => {
   }
 
   return (
-    <Box height="100vh" display="flex" alignItems="center" justifyContent="center" backgroundColor={backgroundColor}>
-      <meta httpEquiv="refresh" content="60"></meta>
-      <Text fontSize="2xl" color="white">
-        {statusText}
-      </Text>
-    </Box>
+    <>
+      <Head>
+        <title>{statusText} - Dylan</title>
+      </Head>
+      <Box height="100vh" display="flex" alignItems="center" justifyContent="center" backgroundColor={backgroundColor}>
+        <meta httpEquiv="refresh" content="60"></meta>
+        <Text fontSize="2xl" color="white">
+          {statusText}
+        </Text>
+      </Box>
+    </>
   );
 };
 
