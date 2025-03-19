f7c45e512c2e2ec62c028d5295ac0a872d19c021
Dylan Watson <lotsabackscatter@gmail.com>
2025-03-19 10:05:31 +0800

    Add a readme page

---
+++ pages/pages/zoom-readme.tsx
@@ -0,0 +1,112 @@
+import { Box, Heading, ListItem, Text, UnorderedList, VStack } from "@chakra-ui/react";
+import axios from "axios";
+import Head from "next/head";
+import Link from "next/link";
+
+const ZoomReadmePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Zoom Readme</title>
+      </Head>
+      <VStack spacing={8} as="main" p={8}>
+        <Box as="section" maxW="2xl" mx="auto" w="100%">
+          <VStack spacing={8} alignItems="center" justifyContent="center" textAlign="center" pt={24} pb={12}>
+            <Box>
+              <Heading
+                as="h1"
+                fontFamily="body"
+                bgColor="blue.400"
+                bgClip="text"
+                size="4xl"
+                bgGradient="linear(to-l, #79c2ff, #4a5888)"
+                className="jumbotron-name-heading"
+              >
+                Zoom Readme
+              </Heading>
+            </Box>
+            <Box>
+              <Heading as="h2" size="lg" lineHeight="tall" color="gray.500" fontWeight="medium">
+                How to know if I'm on a Zoom call
+              </Heading>
+            </Box>
+          </VStack>
+        </Box>
+        <Box as="section" maxW="2xl" mx="auto" w="100%">
+          <Box className="article">
+            <Link href="/pages/zoom" passHref>
+              <a target="_blank" rel="noreferrer">
+                Am I on Zoom?
+              </a>
+            </Link>
+            <br />
+            <br />
+            The following script runs via crontab, every 2 minutes in order to sync the zoom state:
+            <br />
+            <br />
+            file: am-i-on-zoom
+            <pre>
+              {`#!/bin/bash
+
+KV_API_KEY=<key here>
+
+store_kv() {
+    local key="$1"
+    local value="$2"
+    local cache_file="/tmp/store_kv_$\{key}_cache"
+    if [ -z "$key" ] || [ -z "$value" ]; then
+        echo "Usage: store_kv <key> <value>"
+        return 1
+    fi
+    echo "Putting $key: $value"
+
+    # Check if the cache file exists and read its contents
+    if [ -f "$cache_file" ]; then
+        # Read the cached key and value
+        read -r cached_key cached_value < "$cache_file"
+        # If the key and value are the same as the cached ones, do nothing
+        if [ "$key" == "$cached_key" ] && [ "$value" == "$cached_value" ]; then
+            echo "Value unchanged, not updating."
+            return 0
+        fi
+    fi
+
+    # Update the cache file with the new key-value pair
+    echo "$key $value" > "$cache_file"
+    
+    # Perform the update as the value has changed
+    local collection="ddb"
+    local url="https://api.kvstore.io/collections/$collection/items/$key"
+    curl --request PUT "$url" \
+         --header "kvstoreio_api_key: $KV_API_KEY" \
+         --header "Content-Type: text/plain" \
+         --data-raw "$value"
+}
+
+if [ "$1" == "--help" ]; then
+	echo "Usage: am-i-on-zoom"
+else
+	if [ $(/usr/sbin/lsof -i 4UDP | /usr/bin/grep zoom | /usr/bin/awk 'END{print NR}') -gt 1 ]; then
+		echo "Yes, you are on Zoom"
+        store_kv "zoom" "true"
+        
+    else
+        echo "No, you are not on Zoom"
+        store_kv "zoom" "false"
+    fi
+fi`}
+            </pre>
+            <br />
+            <br />
+            Crontab configured:
+            <pre>
+              {`* * * * * $HOME/Workspace/scripts/am-i-on-zoom > $HOME/Workspace/scripts/am-i-on-zoom.log 2>&1`}
+            </pre>
+          </Box>
+        </Box>
+      </VStack>
+    </>
+  );
+};
+
+export default ZoomReadmePage;
--- pages/pages/zoom.tsx
+++ pages/pages/zoom.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from "@chakra-ui/react";
 import axios from "axios";
 import Head from "next/head";
+import Link from "next/link";
 
 const ZoomStatusPage = ({ onZoomCall }: { onZoomCall: string | null }) => {
   let backgroundColor: string;
@@ -34,11 +35,31 @@ const ZoomStatusPage = ({ onZoomCall }: { onZoomCall: string | null }) => {
       <Head>
         <title>{statusText} - Dylan</title>
       </Head>
-      <Box height="100vh" display="flex" alignItems="center" justifyContent="center" backgroundColor={backgroundColor}>
-        <meta httpEquiv="refresh" content="60"></meta>
-        <Text fontSize="2xl" color="white">
-          {statusText}
-        </Text>
+      <Box
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor={backgroundColor}
+      >
+        <meta httpEquiv="refresh" content="60" />
+
+        <Box mb={4}>
+          <Text fontSize="2xl" color="white">
+            {statusText}
+          </Text>
+        </Box>
+
+        <Box>
+          <Text fontSize="sm" color="white">
+            <Link href="/pages/zoom-readme" passHref>
+              <a target="_blank" rel="noreferrer">
+                How does this work?
+              </a>
+            </Link>
+          </Text>
+        </Box>
       </Box>
     </>
   );
